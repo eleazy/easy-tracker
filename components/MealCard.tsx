ed9f7f789e6834dab6fc4684fa5ba83279309080
eleazy <eleazysoares.08@gmail.com>
2024-07-30 20:21:17 -0300

    Inicio da estrutura do aplicativo / Criação dos arquivo do componente MealCard

---
+++ components/MealCard.tsx
@@ -0,0 +1,19 @@
+import { View, Text, StyleSheet, useColorScheme } from 'react-native';
+import React from 'react';
+import { Colors } from '@/constants/Colors';
+
+const MealCard = ( ) => {
+    const colorScheme = useColorScheme() ?? 'light';
+
+  return (
+    <View>
+      <Text style={[{ color: Colors[colorScheme].text }]}>MealCard</Text>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+    
+    });
+
+export default MealCard
